5e8e9be2bcf06c98b9a43a3cbfb429ad57617752
tokyosheep <kawanoshuji@Pro.local>
2019-07-28 21:33:41 +0900

    modify

--- unlockAllSelect/unlockAllSelect.jsx
+++ unlockAllSelect/unlockAllSelect.jsx
@@ -1,19 +1,20 @@
 (function(){
-    layerUnlock(activeDocument);
+    layerUnlock(activeDocument.layers);
     var p = activeDocument.pageItems;
     for(var i=0;i < p.length;i++){
         try{
-            p[i].selected = true;
+            //p[i].selected = true;
         }catch(e){
             p[i].locked = false;
-            p[i].selected = true;
+            //p[i].selected = true;
         }
     }
     function layerUnlock(lay){
-        for(i=0;i<lay.length;i++){
-            lay.layers[i].locked = false;
-            if(lay.layers.length > 0){
-                layerUnlock(lay.layers[i]);
+        for(var i=0;i<lay.length;i++){
+            lay[i].locked = false;
+            $.writeln(lay[i].layers.length);
+            if(lay[i].layers.length > 0){
+                layerUnlock(lay[i].layers);
             }
         }
     }
